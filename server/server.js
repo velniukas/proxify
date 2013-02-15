2005f497b12594938981ddc376f71ff1f3e8a1f4
velniukas <steve.g.messina@gmail.com>
2013-02-16 00:17:04 +0800

    minor fixes to proxy server, not working quite yet

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 var fs = require('fs'),
 	nconf = require('nconf');
-	jasmine = require('jasmine-node'),
 	http = require('http'),
-	https = require('https'),
-	path = require('path'),
-	crypto = require('crypto'),
+//	https = require('https'),
+	//path = require('path'),
+//	crypto = require('crypto'),
 	httpProxy = require('http-proxy');
 
 nconf.argv()
@@ -12,30 +11,33 @@ nconf.argv()
 	 .file( 'server', {	file: 'server.config.json', dir: '../config/' } );
 
 // load the reverse proxy route table 
-var routing = nconf.add( 'routes', { file: 'routes.json', dir: '../config/' } );
+nconf.use( 'file', { file: '../config/routes.json' } );
+console.log("config=\n" + JSON.stringify(nconf.get('router')));
+var options = { router: nconf.get('router') };
 // generic function to load the credentials from disk
-function getCredentialsContext(cert) {
-	return crypto.createCredentials({
-		key: fs.readFileSync(path.join(__dirname, 'certs', cert + '.key')),
-		cert: fs.readFileSync(path.join(__dirname, 'certs', cert + '.crt'))
-	}).context;
-}
+//function getCredentialsContext(cert) {
+//	return crypto.createCredentials({
+//		key: fs.readFileSync(path.join(__dirname, 'certs', cert + '.key')),
+//		cert: fs.readFileSync(path.join(__dirname, 'certs', cert + '.crt'))
+//	}).context;
+//}
 
 // certificate per domain hash - make this configurable 
-var certs = {
-	'foobar.com': getCredentialsContext('foobar'),
-	'barbaz.com': getCredentialsContext('barbaz')
-};
-
-// reverse proxy using routes.json config lookup table 
-var proxyServer = httpProxy.createServer(routing);
-proxyServer.listen(80);
+//var certs = {
+//	'foobar.com': getCredentialsContext('foobar'),
+//	'barbaz.com': getCredentialsContext('barbaz')
+//};
 
 // sample forward proxy e.g. for load balancing
-var proxyForwardServer = httpProxy.createServer(9000, 'localhost', {
-	forward: {
-		port: 9000,
-		host: 'staging.com'
-	}
-});
-proxyForwardServer.listen(80);
+//var proxyForwardServer = httpProxy.createServer(9000, 'localhost', {
+//	forward: {
+//		port: 9000,
+//		host: 'staging.com'
+//	}
+//});
+//proxyForwardServer.listen(80);
+
+// reverse proxy using routes.json config lookup table 
+var proxyServer = httpProxy.createServer(options);
+proxyServer.listen(80);
+
